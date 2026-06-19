e701992bd200b88ed7a1dc6efdfb7c0c1ceba737
DanielQuirozHW <ldquiroz@hwapplications.com>
2026-06-19 13:33:46 -0300

    fix(dtos): add examples to last-active-lesson response dto

--- src/modules/users/dto/last-active-lesson-response.dto.ts
+++ src/modules/users/dto/last-active-lesson-response.dto.ts
@@ -1,9 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class LastActiveLessonResponseDto {
-  @ApiProperty() lessonId!: string;
-  @ApiProperty() moduleId!: string;
-  @ApiProperty() courseId!: string;
-  @ApiProperty() courseSlug!: string;
+  @ApiProperty({ example: 'lesson-uuid' }) lessonId!: string;
+  @ApiProperty({ example: 'module-uuid' }) moduleId!: string;
+  @ApiProperty({ example: 'course-uuid' }) courseId!: string;
+  @ApiProperty({ example: 'introduction-to-typescript' }) courseSlug!: string;
   @ApiProperty() lastWatchedAt!: Date;
 }
